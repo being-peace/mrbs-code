650ea86cde6ac645d3018687d7d5e464be774092
Tobias Polzin <58709258+being-peace@users.noreply.github.com>
2025-10-26 21:39:37 +0100

    roter Hintergrund in der Warnung

--- web/local/custom.js
+++ web/local/custom.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         function checkFields() {
             const destinationField = document.getElementById("name");
             const kmField = document.getElementById("f_custom_km");
+            const zielLabel = document.querySelector("label[for='name']");
             const kmLabel = document.querySelector("label[for='f_custom_km']");
             const destinationValue = destinationField.value.trim().toLowerCase();
             const kmValue = kmField.value;
@@ -17,8 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
             
             if (destinations.includes(destinationValue) && kmValue === kmValueCheck) {
                 kmLabel.textContent = "Fahrtziel und KM Wert passen nicht zusammen!!! - Kilometer pro Tag";
+                zielLabel.textContent = "Fahrtziel und KM Wert passen nicht zusammen!!! - Fahrtziel";
+		zielLabel.style.backgroundColor="red";
+		kmLabel.style.backgroundColor="red";
             } else {
                 kmLabel.textContent = "Kilometer pro Tag";
+                zielLabel.textContent = "Fahrtziel";
+		zielLabel.style.backgroundColor="";
+		kmLabel.style.backgroundColor="";
             }
         }
         
